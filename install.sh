d4480a1f555053a744e051a8646dafe45352dac4
Eric Barendt <ebarendt@gmail.com>
2013-01-16 09:52:41 -0800

    Updated to install the vim ctrl-p plugin

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ if [ ! -f ~/.inputrc -a ! -L ~/.inputrc ]; then
   ln -s inputrc ~/.inputrc
 fi
 
+if [ ! -f ~/.vimrc -a ! -L ~/.vimrc ]; then
+  ln -s vimrc ~/.vimrc
+fi
+
+if [ ! -d ~/.vim/bundle/ctrlp.vim ]; then
+  cd ~/.vim
+  git clone git://github.com/kien/ctrlp.vim.git
+fi 
