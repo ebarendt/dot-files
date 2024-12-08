5c3278be44a52de3b5aa6849c80b0208763d9bff
Eric Barendt <eric@barendt.org>
2024-12-07 16:54:52 -0800

    fedora changes

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ function copy {
   fi
 }
 
-if [ ! -f ~/.zshrc ]; then
-  sudo apt install zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  rm ~/.zshrc
-fi
+#if [ ! -f ~/.zshrc ]; then
+#  sudo apt install zsh
+#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#  rm ~/.zshrc
+#fi
 
 copy profile
 copy irbrc
@@ -23,7 +23,7 @@ copy tmux.conf
 copy inputrc
 copy vimrc
 copy gemrc
-copy zshrc
+#copy zshrc
 copy fzf.zsh
 copy gitignore_global
 
@@ -31,10 +31,10 @@ if [ ! -f ~/.gitconfig ]; then
   cp gitconfig ~/.gitconfig
 fi
 
-if [ ! -d ~/.asdf ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
-  sudo apt-get install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
-fi
+#if [ ! -d ~/.asdf ]; then
+#  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+#  sudo apt-get install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+#fi
 
 if [ ! -d ~/.vim/pack/minpac/opt/minpac ]; then
   git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
